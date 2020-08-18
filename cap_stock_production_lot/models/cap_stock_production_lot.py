9626edb90e57292fdf13992687951c358694e015
captivea-bta <captivea-bta>
2020-08-18 17:12:27 +0530

    [IMP] Updated method to create lot number by making it compatible with other module

--- cap_stock_production_lot/models/cap_stock_production_lot.py
+++ cap_stock_production_lot/models/cap_stock_production_lot.py
@@ -52,20 +52,18 @@ class ProductionLot(models.Model):
             
     @api.model_create_multi
     def create(self, vals_list):
-        
-    
-    
         res = super(ProductionLot,self).create(vals_list)
-        
         #res._set_container_on_ref()
         seq_num = res.name
-        base35 = self.base35encode(int(seq_num))
-        cont = 'D' + base35.zfill(6)
+        if seq_num.isnumeric():
+            base35 = self.base35encode(int(seq_num))
+            cont = 'D' + base35.zfill(6)
+        else:
+            cont = seq_num
         res.write({
             'name': cont,
             'ref': cont,
         })
-        
         return res
             
             
