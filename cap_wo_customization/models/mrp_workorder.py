b33fdd42268fe5e7a34c1433486d2853bc4822de
captivea-bta <captivea-bta>
2020-08-18 17:11:15 +0530

    [IMP] Updated validation on Mark as done button

--- cap_wo_customization/models/mrp_workorder.py
+++ cap_wo_customization/models/mrp_workorder.py
@@ -11,15 +11,15 @@ class MrpProductionWorkcenterLine(models.Model):
     user_ids = fields.Many2many('res.users', string='WO Approved By')
 
     def do_finish(self):
-    	current_user = self.env.user.id
-    	if not self.user_ids:
-    		self.user_ids = [(4, current_user)]
-    	elif self.user_ids and current_user in self.user_ids.ids:
-    		raise ValidationError(_("You already confirmed the WO. Another person " \
-    			"will be able to complete this process"))
-    	elif self.user_ids and len(self.user_ids) < 2 and current_user not in self.user_ids.ids:
-    		self.user_ids = [(4, current_user)]
-    		if len(self.user_ids) >= 2:
-    			return super(MrpProductionWorkcenterLine, self).do_finish()
-    	elif self.user_ids and len(self.user_ids) >= 2:
-    		return super(MrpProductionWorkcenterLine, self).do_finish()
+        current_user = self.env.user.id
+        if self.user_ids and len(self.user_ids) >= 2:
+            return super(MrpProductionWorkcenterLine, self).do_finish()
+        elif not self.user_ids:
+            self.user_ids = [(4, current_user)]
+        elif self.user_ids and current_user in self.user_ids.ids:
+            raise ValidationError(_("You already confirmed the WO. Another person " \
+                "will be able to complete this process"))
+        elif self.user_ids and len(self.user_ids) < 2 and current_user not in self.user_ids.ids:
+            self.user_ids = [(4, current_user)]
+            if len(self.user_ids) >= 2:
+                return super(MrpProductionWorkcenterLine, self).do_finish()
